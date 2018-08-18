509b2ea067d056217867ae10ce4bb4945e1a9cfc
luck <haibh@teusoft.dk>
2018-08-18 23:07:33 +0700

    Blog

---
+++ PropertyPlus/Scripts/home/blogctrl.js
@@ -0,0 +1,11 @@
+﻿function BlogCtrl($scope,
+    $rootScope,
+    $stateParams,
+    $location,
+    $timeout,
+    xhrService,
+    $anchorScroll) {
+    $scope.id = 12;
+}
+
+app.controller('BlogCtrl', BlogCtrl);
