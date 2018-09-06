57e6ff0e906014d0b810614c2186c532e4844182
haibh2705 <haibh2705@gmail.com>
2018-09-06 22:54:28 +0700

    Add API

---
+++ PropertyPlus/Controllers/PropertyPlusController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PropertyPlus.Models;
+
+namespace PropertyPlus.Controllers
+{
+    [RoutePrefix("api/propertyplus")]
+    public class PropertyPlusController : ApiController
+    {
+        [HttpGet]
+        [Route("GetListBlog")]
+        public List<BlogModel> GetListBlog()
+        {
+            return new List<BlogModel>();
+        }
+    }
+}
