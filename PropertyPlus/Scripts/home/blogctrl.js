93d061b3e8653b76ccb221fdfd9f745987fcd674
tuannghia <dinhtuannghia.248@gmail.com>
2018-09-13 21:21:00 -0700

    fix css + api blog-blogdetail-login

--- PropertyPlus/Scripts/home/blogctrl.js
+++ PropertyPlus/Scripts/home/blogctrl.js
@@ -11,27 +11,34 @@
         $location.path("/blog").search({ page: $scope.bigCurrentPage, limit: limit, type: $scope.type, search: $scope.search });
     }
 
-    $scope.loadData = function() {
+    $scope.loadData = function () {
         $scope.bigCurrentPage = $stateParams.page === undefined ? 1 : $stateParams.page;
         $scope.type = $stateParams.type === undefined ? -1 : $stateParams.type;
         $scope.search = $stateParams.search === undefined ? '' : $stateParams.search;
         xhrService.get("GetListBlog/" + $scope.bigCurrentPage + "/" + limit + "/" + $scope.type + "/" + $scope.search)
-            .then(function(data) {
-                    $scope.blogList = data.data.data;
-                },
-                function(error) {
-                    console.log(error.statusText);
-            });
-        xhrService.get("GetSlide/2").then(function (data) {
-                $scope.slideImg = "Upload/" + data.data.Img;
+            .then(function (data) {
+                $scope.blogList = data.data.data;
             },
+                function (error) {
+                    console.log(error.statusText);
+                });
+        xhrService.get("GetSlide/2").then(function (data) {
+            $scope.slideImg = "Upload/" + data.data.Img;
+        },
             function (error) {
                 $scope.errorText = error.statusText;
             });
     };
 
-    $scope.loadDetailData = function(id) {
-        
+    $scope.loadDetailData = function () {
+        var id = $stateParams.id;
+        xhrService.get("GetBlogDetail/" + id).then(function (data) {
+            $scope.blog = data.data;
+            console.log(data);
+        },
+            function (error) {
+                $scope.errorText = error.statusText;
+            });
     };
 }
 
