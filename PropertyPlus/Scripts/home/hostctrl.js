f99adab80638ac14308e32b9f177a52ad15ad443
hsang28999 <sanghd00541@fpt.edu.vn>
2018-09-22 16:12:29 +0700

    fix-step

--- PropertyPlus/Scripts/home/hostctrl.js
+++ PropertyPlus/Scripts/home/hostctrl.js
@@ -6,42 +6,72 @@
     $sce,
     xhrService,
     $anchorScroll) {
-    $scope.data = {};
+    $scope.loadHeaderHost = function () {
+        if (!(localStorage && localStorage.getItem('user_profile'))) {
+            window.location.href = "/";
+        }
+       
+    }
     $scope.apartments = [{ name: "Apartment", value: "1" }];
     $scope.bedrooms = [
-        { name: "1", value: 1 }, { name: "2", value: 2 }, { name: "3", value: 3 }, { name: "4", value: 4 },
-        { name: "5", value: 5 }, { name: "6", value: 6 }
+        { name: "1", value: "1" }, { name: "2", value: "2" }, { name: "3", value: "3" }, { name: "4", value: "4" },
+        { name: "5", value: "5" }, { name: "6", value: "6" }
     ];
     $scope.bathrooms = [
-        { name: "1", value: 1 }, { name: "2", value: 2 }, { name: "3", value: 3 }, { name: "4", value: 4 },
-        { name: "5", value: 5 }, { name: "6", value: 6 }
+        { name: "1", value: "1" }, { name: "2", value: "2" }, { name: "3", value: "3" }, { name: "4", value: "4" },
+        { name: "5", value: "5" }, { name: "6", value: "6" }
     ];
     $scope.loadStep1_1 = function () {
+        $scope.data = {
+            NoBedRoom: "1",
+            NoBathRoom : "1"
+        };
         $scope.projectList = [];
         xhrService.get("GetAllProject/")
             .then(function (data) {
-                    $scope.projectList = data.data;
-                    $scope.data.project_id = "-1";
-                },
+                $scope.projectList = data.data;
+                $scope.data.ProjectId = "-1";
+            },
                 function (error) {
                     console.log(error.statusText);
                 });
-        $scope.data.type = "1";
-       
+        $scope.data.Type = "1";
+
     };
 
-    $scope.loadStep1_2 = function() {
-        $scope.data.NoBedRoom = "1";
-        $scope.data.NoBathRoom = "1";
+    $scope.loadStep1_2 = function () {
+        if ($scope.data == undefined) {
+            $location.url('/host/listing');
+        }
     };
 
     $scope.loadStep2_1 = function () {
+        if ($scope.data == undefined) {
+            $location.url('/host/listing');
+        }
         if ($scope.images == undefined) {
             $scope.images = [];
             $scope.images.push({ file: null, url: null });
         }
-        
+
     }
+    $scope.loadStep1_3 = function() {
+        if ($scope.data == undefined) {
+            $location.url('/host/listing');
+        }
+    }
+    $scope.loadStep4_1 = function() {
+        if ($scope.data == undefined) {
+            $location.url('/host/listing');
+        }
+    }
+
+    $scope.loadStep3_1 = function() {
+        if ($scope.data == undefined) {
+            $location.url('/host/listing');
+        }
+    }
+
     $scope.uploadImg = function (event) {
         if ($scope.images.length < 6) {
             $scope.images.push({ file: null, url: null });
@@ -56,17 +86,15 @@
     //    else return 'service-basic';
     //}
 
-    $scope.submitStep1_1 = function() {
+    $scope.submitStep1_1 = function () {
         $location.url("host/create/step-1-2");
     }
 
-    $scope.loadStep1_2 = function() {
-    };
-
-    $scope.submitStep1_3 = function() {
+    $scope.submitStep1_3 = function () {
         $location.url("host/create/step-1-3");
     };
-    $scope.saveAddress = function(address) {
+    
+    $scope.saveAddress = function (address) {
         $scope.address = address;
         $scope.linkgg = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCWuX4GXgxkGER7KMO5M5NM8npxQ04wxnk&q=" +
             address.lat +
@@ -80,14 +108,10 @@
     }
     $scope.submitAll = function (confirm_img) {
         $scope.confirm_img = confirm_img;
-        $scope.data.NoBedRoom = $scope.data.NoBedRoom.value;
-        $scope.data.NoBathRoom = $scope.data.NoBathRoom.value;
         $scope.data.Address = $scope.address.formattedAddress;
-        $scope.data.Latitude = $scope.address.lat; 
+        $scope.data.Latitude = $scope.address.lat;
         $scope.data.Longitude = $scope.address.lng;
         $scope.data.City = $scope.address.administrative_area_level_1;
-        $scope.data.Type = $scope.apartment.value;
-        $scope.data.ProjectId = $scope.project.value;
         $scope.data.ImgList = [];
         if ($scope.banner_img.url != null) {
             $scope.data.ImgList.push({
@@ -96,14 +120,14 @@
             });
         }
         if ($scope.images.length > 1) {
-            if ($scope.images[$scope.images.length-1].url == null) {
+            if ($scope.images[$scope.images.length - 1].url == null) {
                 $scope.images.splice(-1, 1);
             }
             for (var i = 0; i < $scope.images.length; i++) {
                 var item = {
                     "Img_Base64": $scope.images[i].url,
                     "Type": -1
-            }
+                }
                 $scope.data.ImgList.push(item);
             }
         }
@@ -113,21 +137,14 @@
                 "Type": 2
             });
         }
-        console.log($scope.data);
         xhrService.post("CreateApartment", $scope.data).then(function (data) {
-            console.log(data);
+            $scope.data = undefined;
         }, function (error) {
             $scope.errorText = error.statusText;
         });
     }
 
-    $scope.loadStep1_3 = function () {
-        console.log($scope.apartment);
-    };
-
-    $scope.test = function() {
-        console.log($scope.another_img_1);
-    };
+    
 }
 
 app.controller('HostCtrl', HostCtrl);
